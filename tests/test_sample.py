3a205ce4e1deb551f9001278585d3e01e4afe13e
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2025-04-05 10:51:18 +0530

    session level fixture and xdist

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,32 +1,19 @@
 import pytest
 
+def add(a, b):
+    return a + b
 
-@pytest.mark.usefixtures
-class TestMathOps:
-    def test_add(self, test_meta):
-        p = test_meta["params"]
-        assert p["a"] + p["b"] == p["expected"]
+def subtract(a, b):
+    return a - b
 
-    def test_subtract(self, test_meta):
-        p = test_meta["params"]
-        assert p["a"] - p["b"] == p["expected"]
+@pytest.mark.usefixtures("test_definitions")
+class TestMathOperations:
+    def test_add(self, test_definitions):
+        for case in test_definitions["add"]:
+            a, b, expected = case["a"], case["b"], case["expected"]
+            assert add(a, b) == expected
 
-
-# def test_example(test_meta):
-#     assert "id" in test_meta
-#     print(f"Running test ID: {test_meta['id']}")
-
-
-# def test_addition(test_meta):
-#     a = test_meta["params"]["a"]
-#     b = test_meta["params"]["b"]
-#     expected = test_meta["params"]["expected"]
-#     assert a + b == expected
-
-# Case: Tests Inside a Class
-# This works exactly the same because request.node still points to the current test item, even inside a class.
-# class TestMathOps:
-#     # use class scoped fixture class_meta in the class as test parameter.
-#     def test_add(self, class_meta):
-#         print(f"[Class Scoped] Test ID: {class_meta['id']}")
-
+    def test_subtract(self, test_definitions):
+        for case in test_definitions["subtract"]:
+            a, b, expected = case["a"], case["b"], case["expected"]
+            assert subtract(a, b) == expected
