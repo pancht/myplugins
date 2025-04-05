197cb0efe4f7de841905ac1a1bcd9636b872c985
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2025-04-05 14:46:49 +0530

    refactor code

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import pytest
 import yaml
-import os
 from _pytest.config import Config
 # No need to import when using x-dist
 # from _pytest.nodes import WorkerInput
@@ -14,8 +13,7 @@ def pytest_addoption(parser):
 def pytest_configure(config: Config):
     # Read yaml definitions and share test definitions
     # to workers via config in order to be compatible with pytest-xdist.
-    worker_process: bool = hasattr(config, WORKER_INPUT)
-    if not worker_process:  # master process in pytest-xdist
+    if not worker_process(config):  # master process in pytest-xdist
         yaml_path = config.getoption("--yaml")  # read yaml
         with open(yaml_path, "r") as f:
             config.test_definitions = yaml.safe_load(f)
@@ -23,8 +21,7 @@ def pytest_configure(config: Config):
 @pytest.fixture(scope="session")
 def test_definitions(request):
     config = request.config
-    worker_process: bool = hasattr(config, WORKER_INPUT)
-    if worker_process:
+    if worker_process(config):
         return config.workerinput["test_definitions"]
     return config.test_definitions
 
@@ -32,3 +29,5 @@ def pytest_configure_node(node):
     """This hook is called by the main process to configure a worker node by pytest-xdist plugin."""
     node.workerinput["test_definitions"] = node.config.test_definitions
 
+def worker_process(config: Config) -> bool:
+    return hasattr(config, WORKER_INPUT)
--- doc.md
+++ doc.md
@@ -1,4 +1,4 @@
 -n auto will automatically detect the number of CPU cores and parallelize accordingly.
 `pytest -n auto --yaml=test_data.yml` 
 
-pytest -n 3 --yaml=test_data.yml 
+pytest -n 3 --yaml=tests/test_suite.yml 
