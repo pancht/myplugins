d2e9af5b858519e7d1aff153d60bd9e7035504d1
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2025-04-05 14:38:41 +0530

    using session fixture

--- conftest.py
+++ conftest.py
@@ -13,22 +13,21 @@ def pytest_addoption(parser):
 def pytest_configure(config: Config):
     # Read yaml definitions and share test definitions
     # to workers via config in order to be compatible with pytest-xdist.
-    if hasattr(config, "workerinput"):
-        return  # skip reading yamls if it is a worker node
+    worker_process: bool = hasattr(config, "workerinput")
+    if not worker_process:  # master process in pytest-xdist
+        yaml_path = config.getoption("--yaml")  # read yaml
+        with open(yaml_path, "r") as f:
+            config.test_definitions = yaml.safe_load(f)
 
-    # read yaml definitions only by master node
-    yaml_path = config.getoption("--yaml")
-    with open(yaml_path, "r") as f:
-        config.test_definitions = yaml.safe_load(f)
-
+@pytest.fixture(scope="session")
+def test_definitions(request):
+    config = request.config
+    worker_process: bool = hasattr(config, "workerinput")
+    if worker_process:
+        return config.workerinput["test_definitions"]
+    return config.test_definitions
 
 def pytest_configure_node(node):
     """This hook is called by the main process to configure a worker node by pytest-xdist plugin."""
     node.workerinput["test_definitions"] = node.config.test_definitions
 
-@pytest.fixture(scope="session")
-def test_definitions(request):
-    config = request.config
-    if hasattr(config, "workerinput"):
-        return config.workerinput["test_definitions"]
-    return config.test_definitions
