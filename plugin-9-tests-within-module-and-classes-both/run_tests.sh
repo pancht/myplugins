db18160918ca87484c689f90a0b1a99abcabb999
Panchdev Singh Chauhan <erpanchdev@gmail.com>
2025-04-08 11:41:22 +0530

    fix a glitch in run test

--- plugin-9-tests-within-module-and-classes-both/run_tests.sh
+++ plugin-9-tests-within-module-and-classes-both/run_tests.sh
@@ -8,9 +8,9 @@ if [ -d "allure-report/history" ]; then
 fi
 # run tests with allure
 pytest --data-dir=test_data -n 4 --alluredir=allure-results "$@"
-# generate allure results
-allure generate allure-results -o allure-report --clean
 # overwrite categories.json
 cp .allure/categories.json allure-results/categories.json
+# generate allure results
+allure generate allure-results -o allure-report --clean
 # open allure report (optional)
 allure open allure-report
